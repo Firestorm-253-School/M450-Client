450617835b5c6f77e126a37ce141a18b3dffe65a
Janick Aeschlimann <janick.aeschlimann@bbz-sh.ch>
2026-09-08 15:47:38 +0200

    feat: add background music, refactor ui init

--- src/snake_game_client/ui/ui.py
+++ src/snake_game_client/ui/ui.py
@@ -5,33 +5,14 @@ def test():
    print("Test")
 
 class Ui:
-  def __init__(self):
-
-      
-      self.screen = pygame.display.set_mode((1280, 720))
-      pygame.display.set_caption("Snake Game")
-      self.clock = pygame.time.Clock()
-      pygame.init()
-
-      self.main_menu = MainMenu(self.screen)
+  def __init__(self, screen): 
+      self.main_menu = MainMenu(screen)
 
 
-  def run(self):
+  def handle_event(self, event):
+     self.main_menu.handle_event(event)
 
-    running = True
-    while running:
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              running = False
-
-          self.main_menu.handle_event(event)
-
+  def draw(self, screen): 
       self.main_menu.update()
-      self.main_menu.draw(self.screen)
-
-      pygame.display.flip()
-
-      self.clock.tick(60)
-
-    pygame.quit()
+      self.main_menu.draw(screen)
       
